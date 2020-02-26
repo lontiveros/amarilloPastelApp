a86c272c9291eee1ba8314c3cbc8907a9bdb9d42
Luis Ontiveros <leomkr@gmail.com>
2020-02-25 22:43:07 -0300

    TSK - Se modifica la vista de productos. TSK - Se agregan íconos de productos.

--- src/app/tabs/products/products.component.ts
+++ src/app/tabs/products/products.component.ts
@@ -1,27 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 import { ProductsService } from 'src/app/services/products.service';
+import { Product } from 'src/app/domain/Product';
 
 @Component({
 	selector: 'app-products',
 	templateUrl: './products.component.html',
 	styleUrls: ['./products.component.scss'],
 })
-export class ProductsComponent implements OnInit {
-	public products: Observable<any[]>;
+export class ProductsComponent {
+	public products: Array<Product> = [];
+	private loading;
 
 	constructor(
-		private productService: ProductsService
+		private productService: ProductsService,
+		private loadingController: LoadingController
 	) { }
-
-	ngOnInit() {}
-
+	
 	ionViewWillEnter() {
+		this.initLoading();
 		this.loadProducts();
 	}
-
+	
 	loadProducts() {
-		this.products = this.productService.getProducts();
+		this.productService.getProducts().subscribe(_product => {
+			this.products = _product;
+			this.loadingController.dismiss();
+		});
+	}
+
+	async initLoading() {
+		this.loading = await this.loadingController.create({
+			spinner: "circles",
+			message: "Obteniendo el listado de productos.",
+			translucent: false
+		});
+
+		await this.loading.present();
 	}
 
 }
