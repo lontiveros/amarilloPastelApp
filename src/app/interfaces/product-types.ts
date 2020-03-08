dce3a910fbd350367d58af296893610124381b03
Agustin Pucheta <aguspucheta24293@gmail.com>
2020-03-08 15:56:02 -0300

    add producttype

---
+++ src/app/interfaces/product-types.ts
@@ -0,0 +1,6 @@
+export interface ProductTypes {
+    id: string;
+    title: string;
+    description: string;
+    price: number;
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,14 @@
 export const environment = {
 	production: false,
 	// Paste firebase info.
-	firebase: {}
+	firebase: {
+			apiKey: "AIzaSyDwCohDAqwyT99Ybz127J5WWhlTrbvntGA",
+			authDomain: "amarillopastel-ba.firebaseapp.com",
+			databaseURL: "https://amarillopastel-ba.firebaseio.com",
+			projectId: "amarillopastel-ba",
+			storageBucket: "amarillopastel-ba.appspot.com",
+			messagingSenderId: "1068486477787",
+			appId: "1:1068486477787:web:68b2d765e813a590a749fe",
+			measurementId: "G-BJR2NNPJSM"
+	}
 };
